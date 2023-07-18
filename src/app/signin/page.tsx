89d54c804ce776d7c8d439d6e3dca478fe333d9a
NotSugat <sugatsujakhu@gmail.com>
2023-07-18 13:14:36 +0545

    login ui updated

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,6 +3,7 @@ import React, { FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import { signIn } from "../firebase/auth/auth";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
 
 function SignIn() {
   const [email, setEmail] = React.useState("");
@@ -41,9 +42,11 @@ function SignIn() {
   };
 
   return (
-    <div className="flex h-full items-center justify-center bg-red-400 ">
-      <div className=" rounded-sm bg-black p-4 shadow-sm">
-        <h1 className="text-center text-2xl font-bold">Sign In</h1>
+    <div className="flex h-full items-center justify-center bg-red-400">
+      <div className=" rounded-md bg-black p-4 shadow-md">
+        <h1 className="select-none text-center text-2xl font-bold text-gray-200">
+          Sign In
+        </h1>
         <form onSubmit={handleForm} className="flex flex-col gap-2">
           <label htmlFor="email">
             <p className="text-sm text-gray-200">Email</p>
@@ -66,17 +69,30 @@ function SignIn() {
               name="password"
               id="password"
               placeholder="password"
+              className="w-full rounded-sm bg-slate-300 p-1"
             />
           </label>
-          <button type="submit" className="text-sm text-gray-200">
-            Sign up
+          <button
+            type="submit"
+            className="mt-2 rounded-sm border py-2 text-sm text-gray-300 transition-all duration-150 ease-in-out hover:border-white"
+          >
+            Login
           </button>
         </form>
-        <button onClick={handleGoogleSignIn}>
-          <h2 className="text-2xl font-medium text-gray-200">
-            Sign Up with Google
+        <p className="p-2 text-center text-sm font-medium text-gray-200">OR</p>
+        <button
+          onClick={handleGoogleSignIn}
+          className="flex items-center gap-4 rounded-full border-2 border-gray-300 px-4 py-2 transition-all duration-150 ease-in-out hover:border-white"
+        >
+          <FcGoogle size={24} />
+          <h2 className="text-xl font-medium text-gray-200">
+            Continue with Google
           </h2>
         </button>
+
+        <p className="mt-2 cursor-pointer text-center text-sm text-gray-200 transition-all duration-150 ease-in-out hover:text-white">
+          Not on DoBlog yet? Sign Up
+        </p>
       </div>
     </div>
   );
