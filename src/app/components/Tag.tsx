7aaade80625a87138d9138c65cbf25aaa572eab2
NotSugat <sugatsujakhu@gmail.com>
2023-07-27 23:55:49 +0545

    some changes to search section

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const Tag = ({ tagName }: { tagName: String }) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
   return (
-    <button className="rounded-full bg-gray-200 px-2 py-1 text-sm lg:px-4 lg:py-2  ">
+    <button
+      className={`rounded-full ${
+        isClicked ? "bg-black text-white" : "bg-gray-200"
+      }  px-2 py-1 text-sm transition-all duration-300 ease-in-out hover:translate-y-[-0.5rem] hover:cursor-pointer hover:bg-black hover:text-white lg:px-4 lg:py-2`}
+      onClick={() => setIsClicked(!isClicked)}
+    >
       {tagName}
     </button>
   );
--- src/app/components/search/Search.tsx
+++ src/app/components/search/Search.tsx
@@ -3,21 +3,33 @@ import Tag from "../Tag";
 
 const Search = () => {
   return (
-    <div className="mx-auto my-12 flex max-w-[80%] items-center">
-      <form className="flex items-center gap-2 rounded-full border-2 border-gray-400 px-2">
-        <BiSearchAlt size={24} className="fill-gray-600" />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="h-10 w-full bg-transparent outline-none"
-        />
-      </form>
+    <div className="mx-auto  max-w-[80%] items-center gap-4 space-y-4 border-4 border-gray-200 px-4 py-8">
+      <div>
+        <div className="flex items-center gap-2">
+          <h2 className="text-xl font-medium lg:text-2xl">Discover new </h2>
+          <h2 className="text-4xl font-bold italic">Doer</h2>
+        </div>
+        <p className="text-xl font-semibold italic">
+          Doers & Dreamers, Fear out, dream in. Take action and win!
+        </p>
+      </div>
 
-      <h2>Tags: </h2>
-      <div className="space-x-2">
-        <Tag tagName="Valorant" />
-        <Tag tagName="Mobile legend" />
-        <Tag tagName="Roblox" />
+      {/* search bar */}
+      <div className="flex items-center gap-4">
+        <form className="flex w-96 items-center gap-2 rounded-full border-2 border-gray-400  px-4 py-1">
+          <BiSearchAlt size={24} className="fill-gray-600" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="h-10 w-full bg-transparent outline-none"
+          />
+        </form>
+
+        <div className="space-x-2">
+          <Tag tagName="Valorant" />
+          <Tag tagName="Mobile legend" />
+          <Tag tagName="Roblox" />
+        </div>
       </div>
     </div>
   );
